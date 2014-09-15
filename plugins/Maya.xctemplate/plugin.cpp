e149ac5b50be518e87e2e9759cf349bd4efa83d4
Serguei Kalentchouk <serguei.k@hotmail.com>
2014-09-15 03:03:47 -0700

    fixed indentation to use spaces

--- plugins/Maya.xctemplate/plugin.cpp
+++ plugins/Maya.xctemplate/plugin.cpp
@@ -6,24 +6,24 @@
 MStatus
 initializePlugin(MObject plugin)
 {
-	MStatus status;
+    MStatus status;
     MFnPlugin pluginFn(plugin, "___ORGANIZATIONNAME___",
                        "1.0.0", "Any") ;
     
     // Register elements here
     
-	return status;
+    return status;
 }
 
 MStatus
 uninitializePlugin(MObject plugin)
 {
-	MStatus status;
+    MStatus status;
     MFnPlugin pluginFn(plugin);
     
     // Deregister elements here
     
-	return status;
+    return status;
 }
 // TM and (c) ___YEAR___ ___ORGANIZATIONNAME___. All Rights Reserved.
 // Reproduction in whole or in part without prior written permission of a
